4e454442e78a1bdd5b5f2c7f9d35185a3d52fdcb
yue <yorke5@qq.com>
2024-12-02 10:37:01 +0100

    Create day01.clj

---
+++ day01.clj
@@ -0,0 +1,18 @@
+;part 1
+(let [input (slurp "input")
+      parse #(map read-string (clojure.string/split % #"   "))
+      lines (map parse (clojure.string/split input #"\n"))
+      [l1 l2] (map sort (apply map list lines))
+      res (reduce + (map #(Math/abs (- %1 %2)) l1 l2))
+      ]
+  (println res))
+
+;part 2
+(let [input (slurp "input")
+      parse #(map read-string (clojure.string/split % #"   "))
+      lines (map parse (clojure.string/split input #"\n"))
+      [l1 l2] (apply map list lines)
+      fre (frequencies l2)
+      res (reduce + (map #(* % (get fre % 0)) l1))
+      ]
+  (println res))
