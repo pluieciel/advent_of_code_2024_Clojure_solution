c5b16d0d5d9477c7b82d1f8405772fc1ec5256fe
yue <yorke5@qq.com>
2024-12-03 13:59:06 +0100

    Update day03.clj

--- day03.clj
+++ day03.clj
@@ -5,24 +5,18 @@
       nums (map #(apply * (map read-string (re-seq #"\d+" %))) muls)]
   (println (reduce + nums)))
 
-;part 2
-(let [input (slurp "input")
-      muls-dos-donts (re-seq #"mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)" input)
-      d_flag (atom true)
-      do_n (atom 0)
-      flag #(case %
-              "do()" (do (reset! d_flag true) (swap! do_n inc) [@d_flag @do_n])
-              "don't()" (do (reset! d_flag false) [@d_flag @do_n])
-              [@d_flag @do_n])
-      muls-p (partition-by flag (conj muls-dos-donts "do()"))
-      dos (filter #(= "do()" (first %)) muls-p)
-      parse-mul (fn [mul] (apply * (map read-string (re-seq #"\d+" mul))))
-      cal-do-list (fn [[d & muls]] (reduce + (map parse-mul muls)))
-      res (reduce + (map cal-do-list dos))
-      ]
-  (println res))
+;part 2: regex
+(let [patt #"((?<=^)|(?<=do\(\)))[\s\S]+?((?=$)|(?=don't\(\))|(?=do\(\)))"
+      patt-mul #"mul\(\d{1,3},\d{1,3}\)"
+      parse-mul #(apply * (map read-string (re-seq #"\d+" %)))]
+  (->> (slurp "input")
+    (re-seq patt)
+    (mapcat #(re-seq patt-mul (first %)))
+    (map parse-mul)
+    (reduce +)
+    println))
 
-;part 2, rewrite with ->>
+;part 2, partition-by
 (let [patt #"mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)"
       [d_flag do_n] [(atom true) (atom 0)]
       flag #(case %
