59c7bdb6b81916607c489392b006b4ba34991c08
yue <yorke5@qq.com>
2024-12-03 11:20:47 +0100

    Update day03.clj

--- day03.clj
+++ day03.clj
@@ -20,3 +20,22 @@
       res (reduce + (map cal-do-list dos))
       ]
   (println res))
+
+;part 2, rewrite with ->>
+(let [patt #"mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)"
+      [d_flag do_n] [(atom true) (atom 0)]
+      flag #(case %
+              "do()" (do (reset! d_flag true) (swap! do_n inc) [@d_flag @do_n])
+              "don't()" (do (reset! d_flag false) [@d_flag @do_n])
+              [@d_flag @do_n])
+      parse-mul (fn [mul] (apply * (map read-string (re-seq #"\d+" mul))))
+      cal-do-list (fn [[d & muls]] (reduce + (map parse-mul muls)))
+      res (->> (slurp "input")
+            (re-seq patt)
+            (#(conj % "do()"))
+            (partition-by flag)
+            (filter #(= "do()" (first %)))
+            (map cal-do-list)
+            (reduce +))
+      ]
+  (println res))
