f637477de2ca96a92143911db6fece0d4802cca4
yue <yorke5@qq.com>
2024-12-04 09:08:31 +0100

    Create day04.clj

---
+++ day04.clj
@@ -0,0 +1,35 @@
+;advent-of-code-2024.day-04
+;part 1
+(let [target '(\X \M \A \S)
+      match? #(or (= target %) (= (reverse target) %))
+      find-xmas #(->> % (partition 4 1) (filter match?) count)]
+  (->> (slurp "input")
+    clojure.string/split-lines
+    (#(let [table %]
+      (concat [table (apply map list table)]
+              (for [l4 (partition 4 1 table)]
+                (apply map list (map (fn [n l] (drop n l)) (range 4) l4)))
+              (for [l4 (partition 4 1 (map reverse table))]
+                (apply map list (map (fn [n l] (drop n l)) (range 4) l4))))))
+    (map #(map find-xmas %))
+    (map #(reduce + %))
+    (reduce +)
+    println))
+
+;part 2
+(let [target '(\M \M \S \S)
+      target-set (->> (cycle target) (partition 4 1) (take 4) set)
+      match? (fn [[[a _ b][_ c _][d _ e]]]
+               (and (= c \A)
+                    (target-set (list a b e d))))]
+  (->> (slurp "input")
+    clojure.string/split-lines
+    ((fn [table]
+      (for [l3 (partition 3 1 table)]
+        (->> l3
+          (map #(partition 3 1 %))
+          (apply map list)
+          (filter match?)
+          count))))
+    (reduce +)
+    println))
