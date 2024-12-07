6aa72229c6a9915fe7e711beb7b7194183c5124c
yue <yorke5@qq.com>
2024-12-07 10:56:19 +0100

    Create day07.clj

---
+++ day07.clj
@@ -0,0 +1,27 @@
+;advent-of-code-2024.day-07
+(let [ops [* +]
+      ok? (fn [[f & r] ops]
+            (some #(= f %)
+              (loop [res [(first r)] l (rest r)]
+                (if (empty? l)
+                  res
+                  (recur
+                   (mapcat #(map (fn [op] (op % (first l))) ops) res)
+                   (rest l))))))
+      ops2 [* + (fn [a b] (read-string (str a b)))]
+      parsed (->> (slurp "input")
+               clojure.string/split-lines
+               (map #(map read-string (clojure.string/split % #": | "))))
+  ]
+  ;part 1
+  (->> parsed
+    (filter #(ok? % ops))
+    (map first)
+    (reduce +)
+    println)
+  ;part 2
+  (->> parsed
+    (filter #(ok? % ops2))
+    (map first)
+    (reduce +)
+    println))
