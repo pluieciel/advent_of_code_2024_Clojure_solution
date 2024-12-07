06bcc0556db8ec794ba3195d3fb4c12c0b63339d
yue <yorke5@qq.com>
2024-12-07 11:02:54 +0100

    Update day07.clj

--- day07.clj
+++ day07.clj
@@ -6,7 +6,7 @@
                 (if (empty? l)
                   res
                   (recur
-                   (mapcat #(map (fn [op] (op % (first l))) ops) res)
+                   (mapcat #(map (fn [op] (op % (first l))) ops) (filter #(<= % f) res))
                    (rest l))))))
       ops2 [* + (fn [a b] (read-string (str a b)))]
       parsed (->> (slurp "input")
