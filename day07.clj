05364b9c0bfb37fc515bb7991ab7253ab3342b9e
yue <yorke5@qq.com>
2024-12-07 18:06:32 +0000

    use recursion

--- day07.clj
+++ day07.clj
@@ -2,28 +2,27 @@
 (ns day07
   (:require [clojure.string :as str]))
 
+(defn ok? [[target & lst] ops]
+  (cond
+    (= 1 (count lst)) (= target (first lst))
+    (> (first lst) target) false
+    :else (let [[fi se & re] lst]
+            (some #(ok? (cons target %) ops) (map #(cons (% fi se) re) ops)))))
+
 (let [ops [* +]
-      ok? (fn [[f & r] ops]
-            (some #(= f %)
-              (loop [res [(first r)] l (rest r)]
-                (if (empty? l)
-                  res
-                  (recur
-                   (mapcat #(map (fn [op] (op % (first l))) ops) (filter #(<= % f) res))
-                   (rest l))))))
       ops2 [* + (fn [a b] (read-string (str a b)))]
       parsed (->> (slurp "input")
-               str/split-lines
-               (map #(map read-string (str/split % #": | "))))]
+                  str/split-lines
+                  (map #(map read-string (str/split % #": | "))))]
   ;part 1
   (->> parsed
-    (filter #(ok? % ops))
-    (map first)
-    (reduce +)
-    println)
+       (filter #(ok? % ops))
+       (map first)
+       (reduce +)
+       println)
   ;part 2
   (->> parsed
-    (filter #(ok? % ops2))
-    (map first)
-    (reduce +)
-    println))
+       (filter #(ok? % ops2))
+       (map first)
+       (reduce +)
+       println))
