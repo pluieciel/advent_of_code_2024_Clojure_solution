bff25ede372c0a728a4d2126cc6344ae78121498
yue <yorke5@qq.com>
2024-12-11 11:25:58 +0000

    update day11

--- day11.clj
+++ day11.clj
@@ -2,28 +2,31 @@
 (ns day11
   (:require [clojure.string :as str]))
 
-(def length
+(defn parse [input]
+  (->> (slurp input)
+       (#(str/split % #" "))
+       (map read-string)))
+
+(def calc
   (memoize
    (fn [num step]
      (cond
        (zero? step) 1
-       (zero? num) (length 1 (dec step))
+       (zero? num) (calc 1 (dec step))
        (even? (count (str num))) (let [s (str num)
                                        half (/ (count s) 2)]
-                                   (reduce + (map #(length % (dec step))
-                                                  (map #(Integer/parseInt %)
-                                                       [(subs s 0 half) (subs s half)]))))
-       :else (length (* num 2024) (dec step))))))
+                                   (reduce #(+ %1 (calc (Integer/parseInt %2) (dec step)))
+                                           0 [(subs s 0 half) (subs s half)]))
+       :else (calc (* num 2024) (dec step))))))
+
+(defn calc-all [step lst]
+  (reduce #(+ %1 (calc %2 step)) 0 lst))
 
 ;part 1
-(->> (slurp "input")
-     (#(str/split % #" "))
-     (map read-string)
-     (reduce #(+ %1 (length %2 25)) 0)
+(->> (parse "input")
+     ((partial calc-all 25))
      println)
 ;part 2
-(->> (slurp "input")
-     (#(str/split % #" "))
-     (map read-string)
-     (reduce #(+ %1 (length %2 75)) 0)
+(->> (parse "input")
+     ((partial calc-all 75))
      println)
