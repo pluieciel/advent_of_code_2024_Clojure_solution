2cecd6393ebeade2ce0d896a0a9b15d0beaad886
yue <yorke5@qq.com>
2024-12-16 23:14:01 +0100

    use dijkstra

--- day16.clj
+++ day16.clj
@@ -14,16 +14,16 @@
                                    :when (#{\S \E} (get-in Map [y x]))]
                                {(get-in Map [y x]) [y x]})
                              (apply merge))
-      distE (fn [pos] (->> (map #(Math/abs (- %1 %2)) pos end) (reduce +)))
       dirs [[0 1] [1 0] [0 -1] [-1 0]]
       mymerge (fn [v1 v2]
                 (cond
                   (< (get v1 :score) (get v2 :score)) v1
                   (> (get v1 :score) (get v2 :score)) v2
                   :else (assoc v1 :from (set/union (get v1 :from) (get v2 :from)))))
-      [fin data] (loop [todo [[start 0]] done #{} data {[start 0] {:from #{} :score 0 :exp (distE start)}}]
-                   (let [sortedtodo (sort-by #(get-in data [% :exp]) todo)
-                         [pos ndir :as doing] (first sortedtodo)
+      [fin data] (loop [todo #{[start 0]}
+                        done #{}
+                        data (transient {[start 0] {:from #{} :score 0}})]
+                   (let [[pos ndir :as doing] (apply min-key #(get-in data [% :score]) todo)
                          left [pos (mod (dec ndir) 4)]
                          right [pos (mod (inc ndir) 4)]
                          forw (let [npos (mapv + pos (get dirs ndir))]
@@ -31,27 +31,32 @@
                                            (not (done [npos (mod (+ ndir 2) 4)])))
                                   [npos ndir]))
                          newpos (remove nil? [left right forw])
-                         newdata (apply merge
-                                        (for [k newpos]
-                                          (let [score (+ (get-in data [doing :score])
-                                                         (if (= ndir (k 1)) 1 1000))]
-                                            {k {:from #{doing}
-                                                :score score
-                                                :exp (+ score (distE (k 0)))}})))]
-                     (if (some #(= end (% 0)) newpos)
-                       [forw (merge-with mymerge data newdata)]
+                         curscore (get-in data [doing :score])
+                         newdata (for [k newpos]
+                                   (let [score (+ curscore (if (= ndir (k 1)) 1 1000))]
+                                     [k {:from #{doing} :score score}]))]
+                     ;(println doing)
+                     (if (and forw (= end (forw 0)))
+                       [forw
+                        (reduce #(let [[k v2] %2]
+                                   (if-let [v1 (get %1 k)]
+                                     (assoc! %1 k (mymerge v1 v2))
+                                     (assoc! %1 k v2))) data newdata)]
                        (let [newdone (conj done doing)]
-                         (recur (remove newdone (reduce conj (rest sortedtodo) newpos))
+                         (recur (set/difference (reduce conj (disj todo doing) newpos) newdone)
                                 newdone
-                                (merge-with mymerge data newdata))))))]
+                                (reduce #(let [[k v2] %2]
+                                           (if-let [v1 (get %1 k)]
+                                             (assoc! %1 k (mymerge v1 v2))
+                                             (assoc! %1 k v2))) data newdata))))))]
   ;part 1
   (->> (get-in data [fin :score])
        println)
   ;part 2
-  (->> (loop [todo [fin] res #{}]
+  (->> (loop [todo [fin] res (transient #{})]
          (if (empty? todo)
            (count res)
            (let [doing (first todo)
                  from (get-in data [doing :from])]
-             (recur (reduce conj (rest todo) from) (conj res (doing 0))))))
+             (recur (reduce conj (rest todo) from) (conj! res (doing 0))))))
        println))
