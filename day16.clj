a657bb460e1ce43d4edb8c1b3955f6fbc1a6ce28
yue <yorke5@qq.com>
2024-12-17 16:04:12 +0100

    day17

--- day16.clj
+++ day16.clj
@@ -20,35 +20,40 @@
                   (< (get v1 :score) (get v2 :score)) v1
                   (> (get v1 :score) (get v2 :score)) v2
                   :else (assoc v1 :from (set/union (get v1 :from) (get v2 :from)))))
-      [fin data] (loop [todo #{[start 0]}
+      [fin data] (loop [todo [[start 0]]
                         done #{}
                         data (transient {[start 0] {:from #{} :score 0}})]
-                   (let [[pos ndir :as doing] (apply min-key #(get-in data [% :score]) todo)
-                         left [pos (mod (dec ndir) 4)]
-                         right [pos (mod (inc ndir) 4)]
+                   (let [[pos ndir :as doing] (first todo)
+                         curscore (get-in data [doing :score])
+                         lnpos [pos (mod (dec ndir) 4)]
+                         rnpos [pos (mod (inc ndir) 4)]
+                         revpos [pos (mod (+ ndir 2) 4)]
+                         left (when (not (get done rnpos))
+                                [lnpos {:from #{doing} :score (+ curscore 1000)}])
+                         right (when (not (get done lnpos))
+                                 [rnpos {:from #{doing} :score (+ curscore 1000)}])
                          forw (let [npos (mapv + pos (get dirs ndir))]
                                 (when (and (not= \# (get-in Map npos))
                                            (not (done [npos (mod (+ ndir 2) 4)])))
-                                  [npos ndir]))
-                         newpos (remove nil? [left right forw])
-                         curscore (get-in data [doing :score])
-                         newdata (for [k newpos]
-                                   (let [score (+ curscore (if (= ndir (k 1)) 1 1000))]
-                                     [k {:from #{doing} :score score}]))]
+                                  [[npos ndir] {:from #{doing} :score (inc curscore)}])) 
+                         adddata [forw left right]
+                         newdata (reduce #(if-let [[k v2] %2]
+                                            (if-let [v1 (get %1 k)]
+                                              (assoc! %1 k (mymerge v1 v2))
+                                              (assoc! %1 k v2))
+                                            %1) data adddata)]
                      ;(println doing)
-                     (if (and forw (= end (forw 0)))
-                       [forw
-                        (reduce #(let [[k v2] %2]
-                                   (if-let [v1 (get %1 k)]
-                                     (assoc! %1 k (mymerge v1 v2))
-                                     (assoc! %1 k v2))) data newdata)]
-                       (let [newdone (conj done doing)]
-                         (recur (set/difference (reduce conj (disj todo doing) newpos) newdone)
+                     (if (and forw (= end (ffirst forw)))
+                       [(first forw) newdata]
+                       (let [newdone (conj done doing)
+                             settodo (disj (set todo) doing)]
+                         (recur (->> (rest todo)
+                                     (#(if (and forw (not (settodo (first forw)))) (conj % (first forw)) %))
+                                     (#(if (and right (not (settodo (first right)))) (concat % [(first right)]) %))
+                                     (#(if (and left (not (settodo (first left)))) (concat % [(first left)]) %))
+                                     (remove #(get newdone %)))
                                 newdone
-                                (reduce #(let [[k v2] %2]
-                                           (if-let [v1 (get %1 k)]
-                                             (assoc! %1 k (mymerge v1 v2))
-                                             (assoc! %1 k v2))) data newdata))))))]
+                                newdata)))))]
   ;part 1
   (->> (get-in data [fin :score])
        println)
---
+++ day17.clj
@@ -0,0 +1,63 @@
+;advent-of-code-2024.day-17
+(ns day17
+  (:require [clojure.string :as str]
+            [clojure.set :as set]))
+
+(defn parse [input]
+  (->> (slurp input) (re-seq #"\d+") (map read-string)))
+
+;part 1
+(let [[a b c & lst] (parse "input")
+      [A B C ptr] (map atom [a b c 0])
+      lst (vec lst)
+      out (transient [])
+      comb #(case % 4 @A 5 @B 6 @C 7 "ERROR" %)]
+  (while (get lst @ptr)
+    (let [op (get lst @ptr)
+          va (get lst (inc @ptr))]
+      (case op
+        0 (reset! A (int (/ @A (Math/pow 2 (comb va)))))
+        1 (reset! B (bit-xor (mod @B 8) va))
+        2 (reset! B (mod (comb va) 8))
+        3 (when (not= @A 0) (reset! ptr va))
+        4 (reset! B (bit-xor @B @C))
+        5 (conj! out (mod (comb va) 8))
+        6 (reset! B (int (/ @A (Math/pow 2 (comb va)))))
+        7 (reset! C (int (/ @A (Math/pow 2 (comb va))))))
+      (when (not (and (= op 3) (not= @A 0))) (swap! ptr + 2))))
+  (->> (persistent! out)
+       (str/join ",")
+       println))
+
+;part 2
+;only work for this program:
+;Program: 2,4,1,3,7,5,4,2,0,3,1,5,5,5,3,0
+(defn rev [n]
+  (filter #(let [x (first %)
+                 z (bit-xor (mod x 8) 3)]
+             (= (bit-xor n 5) (mod (bit-xor z (bit-shift-right x z)) 8)))
+          (map #(vector % (bit-and % 2r1111111) (bit-shift-right % 3))
+               (range 1024))))
+
+(defn step [doing lst]
+  (apply concat
+         (for [v doing :when (some #(= (last (last v)) (% 1)) lst)]
+           (let [[_ _ r3] (last v)]
+             (for [nxt (filter #(= r3 (% 1)) lst)]
+               (conj v nxt))))))
+
+(let [[a b c & lst] (parse "input")
+      candi (map rev lst)] 
+  (->> (loop [doing (map #(vector %) (first candi))
+              todo (rest candi)]
+         (if (empty? todo)
+           doing
+           (recur (step doing (first todo)) (rest todo))))
+       (map #(map first %))
+       (map (fn [lst]
+          (loop [todo (rest lst) res (first lst) pos 8]
+            (if (empty? todo)
+              res
+              (recur (rest todo) (+' (mod res pos) (*' (first todo) pos)) (*' pos 8))))))
+       (apply min)
+       println))
