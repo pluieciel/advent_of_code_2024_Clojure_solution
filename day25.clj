563b03ec2895b67890684495a0a0c0a3dc8fea63
yue <yorke5@qq.com>
2024-12-25 09:42:45 +0100

    day25

--- day25.clj
+++ day25.clj
@@ -5,21 +5,18 @@
 (defn parse [input]
   (->> (slurp input)
        (#(str/split % #"\n\n"))
-       (map (fn [m] (->> m
-                         str/split-lines
-                         (#(let [[f & r] %
-                                 lock? (= f "#####")]
-                             (list lock?
-                                   (apply mapv (fn [& l]
-                                                 (let [num (count (filter (fn [x] (= x \#)) l))]
-                                                   (if lock?
-                                                     (- 5 num)
-                                                     num)))
-                                          (butlast r))))))))
-       (group-by first)))
+       (map (fn [one]
+              (let [[f & r] (str/split-lines one)
+                    lock? (= f "#####")]
+                [lock?
+                 (apply mapv
+                        (fn [& l]
+                          (let [num (count (filter #(= % \#) l))]
+                            (if lock? (- 5 num) num)))
+                        (butlast r))])))
+       (group-by first)
+       (reduce-kv #(assoc %1 %2 (map second %3)) {})))
 
-(let [{locks true keys false} (parse "input")
-      locks (map second locks)
-      keys (map second keys)]
-  (->> (for [l locks ] (count (filter #(every? true? (map >= l %)) keys)))
+(let [{locks true keys false} (parse "input")]
+  (->> (for [l locks] (count (filter #(every? true? (map >= l %)) keys)))
        (reduce +)))
