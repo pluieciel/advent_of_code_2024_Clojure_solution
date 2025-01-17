e084522ed868b36856573c0aa1a0075af597c26c
pluieciel <yuzhao@c1r2s3.42luxembourg.lu>
2025-01-17 16:13:43 +0100

    restart

--- 2023/day18.clj
+++ 2023/day18.clj
@@ -1,30 +1,97 @@
 ;advent-of-code-2023.day18
 (ns day18
-  (:require [clojure.string :as str]
-            [clojure.set :as set]))
+  (:require [clojure.string :as str]))
 
 (defn parse [i]
   (->> (slurp i) str/split-lines (map #(str/split % #" "))))
 
 (def dirs {"R" [0 1] "D" [1 0] "L" [0 -1] "U" [-1 0]})
 
-;(read-string "0x70c71")
+(let [actions (parse "./2023/ex18")
+      start [0 0]
+      path (->> (reduce (fn [done action]
+                          (let [pos (last done)
+                                [d step _] action
+                                step (read-string step)
+                                newpos (mapv + pos (map #(* % step) (get dirs d)))]
+                            (conj done newpos)))
+                        [start] actions)
+                rest)
+      rows (->> path
+                (group-by first)
+                (#(for [[r lst] %] [r (partition 2 (sort (map second lst)))]))
+                (sort-by first))]
+  rows
+  )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 (let [edge (loop [todo (parse "./2023/ex18") pos [0 0] done #{[0 0]}]
+             (println pos)
              (if (empty? todo)
                done
-               (let [[_ _ color] (first todo)
-                     step (read-string (str "0x" (subs color 2 7)))
-                     dir (get (vec (vals dirs)) (read-string (subs color 7 8)))
+               (let [;[_ _ color] (first todo)
+                     ;step (read-string (str "0x" (subs color 2 7)))
+                     ;dir (get (vec (vals dirs)) (read-string (subs color 7 8)))
+                     [d step _] (first todo)
+                     step (read-string step)
+                     dir (get dirs d)
                      newpos (mapv + pos (map (fn [n] (* n step)) dir))]
                  (recur (rest todo) newpos (conj done newpos)))))
-      edge (sort-by first (group-by first edge))
+      edge (for [[r lst] (sort-by first (group-by first edge))] [r (partition 2 (sort-by last lst))])
       ]
-  (loop [todo edge curr [] tot 0]
+  edge
+  (defn get-len [lst]
+    (->> lst
+         (map (fn [[[_ a] [_ b]]] (inc (- b a))))
+         (reduce +)))
+  
+  (loop [todo (rest edge) curr (second (first edge)) tot 0 prerow (first (first edge)) prelen 0]
+    (println curr tot prerow prelen)
     (if (empty? todo)
-      (+ tot (get-len curr))
-      (let [doing])))
-  )
+      tot
+      (let [[row lst] (first todo)
+            len (get-len curr)
+            toadd (* (- row prerow) len)
+            [newrow mu] (loop [res [] now curr nxt lst makeup 0]
+                          (println "*" res now nxt makeup)
+                          (if (empty? nxt)
+                            [(concat res now) makeup]
+                            (let [now (if-let [newnow (and (> (count now) 1)
+                                                           (let [[[a b] [c d] & r] now]
+                                                             (if (= (last b) (last c)) (do (println "**" (conj r [a d])) (conj r [a d])) now)))]
+                                        newnow
+                                        now)
+                                  [s e :as old] (first now) [ns ne :as new] (first nxt)]
+                              (println "***" res now nxt makeup)
+                              (cond
+                                (empty? now) (recur (vec (concat res nxt)) now '() makeup)
+                                (< (last ne) (last s)) (recur (conj res new) now (rest nxt) makeup)
+                                (< (last e) (last ns)) (recur (conj res old) (rest now) nxt makeup)
+                                (and (= (last s) (last ns)) (= (last e) (last ne))) (recur res (rest now) (rest nxt) (+ makeup (inc (- (last ne) (last ns)))))
+                                (= (last s) (last ns)) (recur res (conj (rest now) [ne e]) (rest nxt) (+ makeup (- (last ne) (last ns))))
+                                (= (last s) (last ne)) (recur res (conj (rest now) [ns e]) (rest nxt) makeup)
+                                (= (last e) (last ne)) (recur res (conj (rest now) [s ns]) (rest nxt) (+ makeup (- (last ne) (last ns))))
+                                (= (last e) (last ns)) (recur res (conj (rest now) [s ne]) (rest nxt) makeup)
+                                (apply < (map last [s ns ne e])) (recur (conj res [s ns]) (conj (rest now) [ne e]) (rest nxt) (+ makeup (- (last ns) (last s)) (- (last e) (last ne))))))))]
+        ;(println mu)
+        (recur (rest todo) newrow (+ tot toadd mu) row len)))))
 
 (def dirs {"R" [0 1] "D" [1 0] "L" [0 -1] "U" [-1 0]})
 
