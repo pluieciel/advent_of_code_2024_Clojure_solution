4df4fd9d3b2ca82c08cdc0e51ecec77d15937e6f
yue <yorke5@qq.com>
2025-01-21 22:19:20 +0100

    2023day23half

---
+++ 2023/day23.clj
@@ -0,0 +1,77 @@
+;advent-of-code-2023.day23
+(ns day23
+  (:require [clojure.string :as str]
+            [clojure.data.priority-map :refer [priority-map]]))
+
+(defn parse [i]
+  (->> (slurp i)
+       str/split-lines
+       (mapv vec)))
+
+(let [Map (parse "./2023/in23")
+      h (count Map) w (count (first Map))
+      [start] (for [x (range w) :when (= \. (get-in Map [0 x]))] [0 x])
+      [end] (for [x (range w) :when (= \. (get-in Map [(dec h) x]))] [(dec h) x])
+      visited (atom #{})]
+  (def dp
+    (memoize
+     (fn [[y x :as pos]]
+       ;(println y x)
+       (let [mark (get-in Map pos)]
+         (swap! visited conj pos)
+         (let [res (cond
+                     (= pos start) 0
+                     (= mark \#) -1000
+                     :else (let [up [(dec y) x]
+                                 fup (if (and (not (@visited up)) (not= \# (get-in Map up \#))) (inc (dp up)) -1000)
+                                 down [(inc y) x]
+                                 fdown (if (and (not (@visited down)) (not= \# (get-in Map up \#))) (inc (dp down)) -1000)
+                                 left [y (dec x)]
+                                 fleft (if (and (not (@visited left)) (not= \# (get-in Map up \#))) (inc (dp left)) -1000)
+                                 right [y (inc x)]
+                                 fright (if (and (not (@visited right)) (not= \# (get-in Map up \#))) (inc (dp right)) -1000)]
+                             (max fup fdown fleft fright)))]
+                                   ;(println pos res)
+           (swap! visited disj pos)
+           res)))))
+  (dp end))
+
+(let [Map (parse "./2023/in23")
+      h (count Map) w (count (first Map))
+      [start] (for [x (range w) :when (= \. (get-in Map [0 x]))] [0 x])
+      [end] (for [x (range w) :when (= \. (get-in Map [(dec h) x]))] [(dec h) x])
+      visited (atom #{})]
+  (def dp
+    (memoize
+     (fn [[y x :as pos]]
+       ;(println y x)
+       (let [mark (get-in Map pos)]
+         (swap! visited conj pos)
+         (let [res (cond
+                     (= pos start) 0
+                     (= mark \#) -1000
+                     (= mark \.) (let [up [(dec y) x]
+                                       fup (if (and (not (@visited up)) (#{\. \v} (get-in Map up))) (inc (dp up)) -1000)
+                                       down [(inc y) x]
+                                       fdown (if (and (not (@visited down)) (#{\. \^} (get-in Map down))) (inc (dp down)) -1000)
+                                       left [y (dec x)]
+                                       fleft (if (and (not (@visited left)) (#{\. \>} (get-in Map left))) (inc (dp left)) -1000)
+                                       right [y (inc x)]
+                                       fright (if (and (not (@visited right)) (#{\. \<} (get-in Map right))) (inc (dp right)) -1000)]
+                                   (max fup fdown fleft fright))
+                     (= mark \>) (let [left [y (dec x)]
+                                       fleft (if (and (not (@visited left)) (#{\. \>} (get-in Map left))) (inc (dp left)) -1000)]
+                                   (max fleft))
+                     (= mark \<) (let [right [y (inc x)]
+                                       fright (if (and (not (@visited right)) (#{\. \<} (get-in Map right))) (inc (dp right)) -1000)]
+                                   (max fright))
+                     (= mark \v) (let [up [(dec y) x]
+                                       fup (if (and (not (@visited up)) (#{\. \v} (get-in Map up))) (inc (dp up)) -1000)]
+                                   (max fup))
+                     (= mark \^) (let [down [(inc y) x]
+                                       fdown (if (and (not (@visited down)) (#{\. \^} (get-in Map down))) (inc (dp down)) -1000)]
+                                   (max fdown)))]
+                                   ;(println pos res)
+           (swap! visited disj pos)
+           res)))))
+  (dp end))
--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
                  [org.clojure/core.logic "1.1.0"]
                  [org.clojure/core.async "1.5.648"]
                  [org.flatland/ordered "1.5.9"]
-                 [org.clojure/data.priority-map "1.2.0"]])
+                 [org.clojure/data.priority-map "1.2.0"]]
+  :jvm-opts ["-Xss6144k"])
