ac6ceb861bbeb5c0441486940261a600f62242b8
yue <yorke5@qq.com>
2025-01-26 14:41:59 +0100

    2023day24

--- 2023/day24.clj
+++ 2023/day24.clj
@@ -33,24 +33,6 @@
                 (recur nxt (+ cnt res)))))))))
 
 ;part 2
-(defn myabs [x]
-  (if (neg? x) (- x) x))
-
-(defn gcd [a b]
-  (if (zero? b)
-    (myabs a) ; GCD is always non-negative
-    (gcd b (mod a b))))
-
-(defn multi-gcd [nums]
-  (reduce gcd (map myabs nums)))
-
-(defn normalize-direction [dir]
-  (let [d (multi-gcd dir)
-        [x y z] dir]
-    (when (zero? d) ; Handle invalid zero direction
-      (throw (IllegalArgumentException. "Direction vector cannot be zero.")))
-    [(quot x d) (quot y d) (quot z d)]))
-
 (defn cross [[x1 y1 z1] [x2 y2 z2]]
   [(- (*' y1 z2) (*' z1 y2))    ; x-component: y1*z2 - z1*y2
    (- (*' z1 x2) (*' x1 z2))    ; y-component: z1*x2 - x1*z2
@@ -62,8 +44,7 @@
        (map #(map - % v0)) ;; shift system relative to stone0
        ((fn [[_ [x1 y1 z1 vx1 vy1 vz1] [x2 y2 z2 vx2 vy2 vz2]]]
           (let [[a b c] (->> (cross (cross [x1 y1 z1] [vx1 vy1 vz1])
-                                    (cross [x2 y2 z2] [vx2 vy2 vz2]))
-                             normalize-direction) ;; get rock direction (must go through origin)
+                                    (cross [x2 y2 z2] [vx2 vy2 vz2]))) ;; get rock direction (must go through origin)
                 r1 (/ (- (* vx1 y1) (* vy1 x1)) (- (* vx1 b) (* vy1 a)))
                 hit1 [(* r1 a) (* r1 b) (* r1 c)]
                 t1 (repeat 3 (/ (- (* r1 a) x1) vx1))
