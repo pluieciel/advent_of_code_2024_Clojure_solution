29683e7441d2ebd6e84c5d77fc6935dd4533d674
yue <yorke5@qq.com>
2025-01-28 17:21:14 +0100

    2022day07

--- 2022/day07.clj
+++ 2022/day07.clj
@@ -1,11 +1,11 @@
 ;advent-of-code-2022.day07
 (ns day07
   (:require [clojure.string :as str]))
-;part 1
+
 (let [data (str/split-lines (slurp "./2022/in07"))
       dict (atom {})
       path (atom [])
-      res (atom [])]
+      res (atom {})]
   (doseq [line data]
     (let [lst (str/split line #" ")]
       (case (first lst)
@@ -20,37 +20,16 @@
     (let [lst (get-in @dict path)]
       (if (number? lst)
         lst
-        (let [size (reduce + (map (fn [[k _]] (caldir (conj path k))) lst))]
-          (when (<= size 100000)
-            (swap! res conj size))
-          size))))
-  (caldir ["/"])
-  (->> @res
-       (reduce +)))
-;part 2
-(let [data (str/split-lines (slurp "./2022/in07"))
-      dict (atom {})
-      path (atom [])
-      res (atom {})]
-  (doseq [line data]
-    (let [lst (str/split line #" ")]
-      (case (first lst)
-        "$" (case (second lst)
-              "cd" (if (= ".." (last lst))
-                     (swap! path pop)
-                     (swap! path conj (last lst)))
-              "ls" nil)
-        "dir" (swap! dict assoc-in (conj @path (second lst)) {})
-        (let [num (read-string (first lst))]
-          (swap! dict assoc-in (conj @path (second lst)) num))))) 
-  (defn caldir [path]
-    (let [lst (get-in @dict path)]
-      (if (number? lst)
-        lst
-        (let [size (reduce + (map (fn [[k _]] (caldir (conj path k))) lst))]
+        (let [size (reduce + (map (fn [[k _]] (caldir (conj path k))) lst))] 
           (swap! res assoc path size)
           size))))
   (caldir ["/"])
+  ;part 1
+  (->> @res
+       vals
+       (filter #(<= % 100000))
+       (reduce +))
+  ;part 2
   (let [total 70000000
         target 30000000
         curr (@res ["/"])
