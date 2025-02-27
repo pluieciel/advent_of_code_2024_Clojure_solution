cc28ffba747355d0259fc435d98d75b332b9c376
pluieciel <yorke5@qq.com>
2025-02-27 20:48:19 +0100

    2022day20

--- 2022/day20.clj
+++ 2022/day20.clj
@@ -1,13 +1,13 @@
 ;advent-of-code-2022.day20
 (ns day20
-  (:require [clojure.string :as str]
-            [clojure.data.finger-tree :as ft]))
+  (:require [clojure.string :as str]))
 
 (defn parse [i]
   (->> (slurp i)
        str/split-lines
        (map read-string)))
 
+;part 1
 (let [arr (parse "2022/in20")
       len (count arr)
       i_arr (map-indexed (fn [idx v] [idx v]) arr)
@@ -28,6 +28,45 @@
                                     (conj
                                      (drop newpos newl)
                                      [idx v])))))))))
+      final (mapv second res)
+      idx-z (first (for [i (range len) :when (zero? (get final i))] i))
+      idx [1000 2000 3000]]
+  (->> idx
+       (map #(get final (mod (+ idx-z %) len)))
+       (reduce +)))
+
+;part 2
+(let [arr (parse "2022/in20")
+      len (count arr)
+      cst 811589153
+      i_arr (map-indexed (fn [idx v] [idx v]) (map #(* % cst) arr))
+      get_state_idx (fn [idx state]
+                      (loop [i 0]
+                        (cond
+                          (= i len) (throw (Exception. "idx not found"))
+                          (= idx (first (state i))) i
+                          :else (recur (inc i)))))
+      res (loop [time 0 temp (vec i_arr)]
+            (if (= time 10)
+              temp
+              (recur
+               (inc time)
+               (loop [doing 0 state temp]
+                 (if (= doing len)
+                   state
+                   (let [state_idx (get_state_idx doing state)
+                         [idx v] (get state state_idx)
+                         newpos (mod (+ state_idx v) (dec len))
+                         newl (concat (take state_idx state)
+                                      (drop (inc state_idx) state))
+                         newstate (vec
+                                   (concat
+                                    (take newpos newl)
+                                    (conj
+                                     (drop newpos newl)
+                                     [idx v])))
+                         newdoing (inc doing)]
+                     (recur newdoing newstate)))))))
       final (mapv second res)
       idx-z (first (for [i (range len) :when (zero? (get final i))] i))
       idx [1000 2000 3000]]
