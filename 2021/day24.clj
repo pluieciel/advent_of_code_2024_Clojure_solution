13de06caf73d21e2fa576f843f3a8ce6f2a964ba
pluieciel <yorke5@qq.com>
2025-04-07 20:20:31 +0200

    2021day24

--- 2021/day24.clj
+++ 2021/day24.clj
@@ -5,176 +5,80 @@
 (defn grow [z i c]
   (+ (* 26 z) i c))
 
+(declare cando cando2 div div2 mul mul2)
+
+;part 1
+(defn div [idx lst z n]
+  (let [i (- (mod z 26) n)]
+    (if (<= 1 i 9)
+      (if-let [newlst (cando (inc idx) (quot z 26) (conj lst i))]
+        newlst
+        nil)
+      nil)))
+
+(defn mul [idx lst z n]
+  (loop [i 9]
+    (if (zero? i)
+      nil
+      (if-let [newlst (cando (inc idx) (grow z i n) (conj lst i))]
+        newlst
+        (recur (dec i))))))
+
 (defn cando [idx z lst]
   (case idx
     14 (let [i (- (mod z 26) 8)]
          (if (<= 1 i 9) (conj lst i) nil))
-    13 (let [i (- (mod z 26) 4)]
-         (if (<= 1 i 9)
-           (if-let [newlst (cando (inc idx) (quot z 26) (conj lst i))]
-             newlst
-             nil)
-           nil))
-    12 (let [i (- (mod z 26) 5)]
-         (if (<= 1 i 9)
-           (if-let [newlst (cando (inc idx) (quot z 26) (conj lst i))]
-             newlst
-             nil)
-           nil))
-    11 (loop [i 9]
-         (if (zero? i)
-           nil
-           (if-let [newlst (cando (inc idx) (grow z i 7) (conj lst i))]
-             newlst
-             (recur (dec i)))))
-    10 (let [i (- (mod z 26) 1)]
-         (if (<= 1 i 9)
-           (if-let [newlst (cando (inc idx) (quot z 26) (conj lst i))]
-             newlst
-             nil)
-           nil))
-    9 (loop [i 9]
-        (if (zero? i)
-          nil
-          (if-let [newlst (cando (inc idx) (grow z i 6) (conj lst i))]
-            newlst
-            (recur (dec i)))))
-    8 (let [i (- (mod z 26) 13)]
-        (if (<= 1 i 9)
-          (if-let [newlst (cando (inc idx) (quot z 26) (conj lst i))]
-            newlst
-            nil)
-          nil))
-    7 (loop [i 9]
-        (if (zero? i)
-          nil
-          (if-let [newlst (cando (inc idx) (grow z i 7) (conj lst i))]
-            newlst
-            (recur (dec i)))))
-    6 (let [i (- (mod z 26) 5)]
-        (if (<= 1 i 9)
-          (if-let [newlst (cando (inc idx) (quot z 26) (conj lst i))]
-            newlst
-            nil)
-          nil))
-    5 (let [i (- (mod z 26) 8)]
-        (if (<= 1 i 9)
-          (if-let [newlst (cando (inc idx) (quot z 26) (conj lst i))]
-            newlst
-            nil)
-          nil))
-    4 (loop [i 9]
-        (if (zero? i)
-          nil
-          (if-let [newlst (cando (inc idx) (grow z i 11) (conj lst i))]
-            newlst
-            (recur (dec i)))))
-    3 (loop [i 9]
-        (if (zero? i)
-          nil
-          (if-let [newlst (cando (inc idx) (grow z i 1) (conj lst i))]
-            newlst
-            (recur (dec i)))))
-    2 (loop [i 9]
-        (if (zero? i)
-          nil
-          (if-let [newlst (cando (inc idx) (grow z i 11) (conj lst i))]
-            newlst
-            (recur (dec i)))))
-    1 (loop [i 9]
-        (if (zero? i)
-          nil
-          (if-let [newlst (cando (inc idx) (grow z i 1) (conj lst i))]
-            newlst
-            (recur (dec i)))))))
+    13 (div idx lst z 4)
+    12 (div idx lst z 5)
+    11 (mul idx lst z 7)
+    10 (div idx lst z 1)
+    9 (mul idx lst z 6)
+    8 (div idx lst z 13)
+    7 (mul idx lst z 7)
+    6 (div idx lst z 5)
+    5 (div idx lst z 8)
+    4 (mul idx lst z 11)
+    3 (mul idx lst z 1)
+    2 (mul idx lst z 11)
+    1 (mul idx lst z 1)))
 
-;part 1
 (->> (cando 1 0 [])
      (str/join ""))
 
 ;part 2
+(defn div2 [idx lst z n]
+  (let [i (- (mod z 26) n)]
+    (if (<= 1 i 9)
+      (if-let [newlst (cando2 (inc idx) (quot z 26) (conj lst i))]
+        newlst
+        nil)
+      nil)))
+
+(defn mul2 [idx lst z n]
+  (loop [i 1]
+    (if (= 10 i)
+      nil
+      (if-let [newlst (cando2 (inc idx) (grow z i n) (conj lst i))]
+        newlst
+        (recur (inc i))))))
+
 (defn cando2 [idx z lst]
   (case idx
     14 (let [i (- (mod z 26) 8)]
          (if (<= 1 i 9) (conj lst i) nil))
-    13 (let [i (- (mod z 26) 4)]
-         (if (<= 1 i 9)
-           (if-let [newlst (cando2 (inc idx) (quot z 26) (conj lst i))]
-             newlst
-             nil)
-           nil))
-    12 (let [i (- (mod z 26) 5)]
-         (if (<= 1 i 9)
-           (if-let [newlst (cando2 (inc idx) (quot z 26) (conj lst i))]
-             newlst
-             nil)
-           nil))
-    11 (loop [i 1]
-         (if (= 10 i)
-           nil
-           (if-let [newlst (cando2 (inc idx) (grow z i 7) (conj lst i))]
-             newlst
-             (recur (inc i)))))
-    10 (let [i (- (mod z 26) 1)]
-         (if (<= 1 i 9)
-           (if-let [newlst (cando2 (inc idx) (quot z 26) (conj lst i))]
-             newlst
-             nil)
-           nil))
-    9 (loop [i 1]
-        (if (= 10 i)
-          nil
-          (if-let [newlst (cando2 (inc idx) (grow z i 6) (conj lst i))]
-            newlst
-            (recur (inc i)))))
-    8 (let [i (- (mod z 26) 13)]
-        (if (<= 1 i 9)
-          (if-let [newlst (cando2 (inc idx) (quot z 26) (conj lst i))]
-            newlst
-            nil)
-          nil))
-    7 (loop [i 1]
-        (if (= 10 i)
-          nil
-          (if-let [newlst (cando2 (inc idx) (grow z i 7) (conj lst i))]
-            newlst
-            (recur (inc i)))))
-    6 (let [i (- (mod z 26) 5)]
-        (if (<= 1 i 9)
-          (if-let [newlst (cando2 (inc idx) (quot z 26) (conj lst i))]
-            newlst
-            nil)
-          nil))
-    5 (let [i (- (mod z 26) 8)]
-        (if (<= 1 i 9)
-          (if-let [newlst (cando2 (inc idx) (quot z 26) (conj lst i))]
-            newlst
-            nil)
-          nil))
-    4 (loop [i 1]
-        (if (= 10 i)
-          nil
-          (if-let [newlst (cando2 (inc idx) (grow z i 11) (conj lst i))]
-            newlst
-            (recur (inc i)))))
-    3 (loop [i 1]
-        (if (= 10 i)
-          nil
-          (if-let [newlst (cando2 (inc idx) (grow z i 1) (conj lst i))]
-            newlst
-            (recur (inc i)))))
-    2 (loop [i 1]
-        (if (= 10 i)
-          nil
-          (if-let [newlst (cando2 (inc idx) (grow z i 11) (conj lst i))]
-            newlst
-            (recur (inc i)))))
-    1 (loop [i 1]
-        (if (= 10 i)
-          nil
-          (if-let [newlst (cando2 (inc idx) (grow z i 1) (conj lst i))]
-            newlst
-            (recur (inc i)))))))
+    13 (div2 idx lst z 4)
+    12 (div2 idx lst z 5)
+    11 (mul2 idx lst z 7)
+    10 (div2 idx lst z 1)
+    9 (mul2 idx lst z 6)
+    8 (div2 idx lst z 13)
+    7 (mul2 idx lst z 7)
+    6 (div2 idx lst z 5)
+    5 (div2 idx lst z 8)
+    4 (mul2 idx lst z 11)
+    3 (mul2 idx lst z 1)
+    2 (mul2 idx lst z 11)
+    1 (mul2 idx lst z 1)))
 
 (->> (cando2 1 0 [])
      (str/join ""))
