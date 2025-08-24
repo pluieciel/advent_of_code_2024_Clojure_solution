f007859e0a42ec174a54351958d31c8c296d42b5
pluieciel <yorke5@qq.com>
2025-08-24 16:02:44 +0000

    2019day04

---
+++ 2019/day04.clj
@@ -0,0 +1,30 @@
+;advent-of-code-2019.day04
+(ns day04
+  (:require [clojure.string :as str]
+            [clojure.set :as set]))
+
+(let [[s e] (->> (slurp "2019/in04")
+                 (re-seq #"\d+")
+                 (map read-string))]
+  (def lower s)
+  (def upper e))
+
+;; part1
+(->> (for [n (range lower (inc upper))
+           :let [lst (->> n str (map #(- (int %) (int \0))))]
+           :when (and (apply <= lst)
+                      (->> lst
+                           (partition 2 1)
+                           (some #(apply = %))))]
+       n)
+     count)
+
+;; part2
+(->> (for [n (range lower (inc upper))
+           :let [lst (->> n str (map #(- (int %) (int \0))))]
+           :when (and (apply <= lst)
+                      (->> lst
+                           (partition-by identity)
+                           (some #(= 2 (count %)))))]
+       n)
+     count)
